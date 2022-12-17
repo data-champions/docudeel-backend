1f07722453838e254679bb791e3b8ebfe3e044fd
david-fortini <fortini.david@gmail.com>
2022-12-17 19:39:32 +0100

    fixed bugs and added functionality, TODO add unit tests

--- src/app.py
+++ src/app.py
@@ -7,6 +7,25 @@ from werkzeug.utils import secure_filename
 
 from db import debiteur_nummer_exist
 from response import get_response
+import requests
+from datetime import datetime
+
+
+def make_filename(debiteur_nummer: str,
+                  description: str,
+                  file_ext: str) -> str:
+    now = str(datetime.now())[:19]
+    fn = f"{debiteur_nummer}_{description}_{now}{file_ext}"
+    fn = fn.replace(' ', '_').replace(':', '_')
+    return fn
+
+## TODO unit test
+# no : in filename
+# no space in filename
+# no . in filename
+# no .. before extension
+
+
 
 logging.basicConfig(format='%(asctime)s:%(levelname)s:%(filename)s:%(funcName)s:%(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
@@ -18,7 +37,7 @@ MAX_MB_REQUEST = 100
 app.config['MAX_CONTENT_LENGTH'] = MAX_MB_REQUEST * 1024 * 1024
 CORS(app)
 
-
+URL_MAKE = 'https://hook.eu1.make.com/wqy2x2k2owdng5ldqkr5d8fcvu95itu3'
 @app.route('/', methods=['POST'])
 def upload_files():
     try:
@@ -28,38 +47,43 @@ def upload_files():
         description = request.form.get('description')
         lang = request.form.get('lang', 'en')
         logging.info(f'{request.form=}')
-        # TODO IN FRONTEND!
-        # if 'file' not in request.files:
-        #     flash('No file part')
-        #     logging.info("'No file part'...")
-        #     resp = get_response('nofile', lang=lang)
-        #     return make_response(jsonify(resp), 400)
         if user_id is None or email is None or description is None:
-            # TODO remove and do in frontend
             resp = dict(message="Param user_id, email or description is missing")
             return make_response(jsonify(resp), 400)
         
         clean_user_id = user_id.replace(' ', '').replace('-', '')
-        is_client = debiteur_nummer_exist(user_id)
+        is_client = debiteur_nummer_exist(clean_user_id)
         if not is_client:
             logging.info(f"{user_id=} {clean_user_id=} not found in records!")
             resp = get_response(response_type='debitnummer_notfound', lang=lang)
             return make_response(jsonify(resp), 400)
         file = request.files['file']
+        print(type(file))
         # obtaining the name of the destination file
-        filename = file.filename
-        if filename == '':
+        original_fn = file.filename
+        if original_fn == '':
             logging.info('Invalid file')
             resp = dict(message="No file selected for uploading")
             return make_response(jsonify(resp), 400)
         else:
-            logging.info('Selected file is= [%s]', filename)
-            file_ext = os.path.splitext(filename)[1]
-            # TODO
-            url = 'https://hook.eu1.make.com/wqy2x2k2owdng5ldqkr5d8fcvu95itu3'
-            
-            logging.info('Upload is successful')
-            resp = get_response(response_type='ok', lang=lang)
+            logging.info('Selected file is= [%s]', original_fn)
+            file_ext = os.path.splitext(original_fn)[1]
+            upload_file = file.stream
+            files = {'upload_file': upload_file}
+            filename = make_filename(debiteur_nummer=clean_user_id,
+                                     description=description,
+                                     file_ext=file_ext)
+            data = {'filename': filename, "file": files}
+            print(f'{data=}')
+            print(type(upload_file))
+            r = requests.post(URL_MAKE, files=files,
+                              data=data)
+            logging.info(f'{r=}')
+            print(f'{r.status_code=}')
+            print(f'{r.text=}')
+
+            resp = get_response(response_type='ok', lang=lang, 
+                                original_filename=original_fn)
             return make_response(jsonify(resp), 200)
     except:
         resp = get_response(response_type='fallback', lang=lang)

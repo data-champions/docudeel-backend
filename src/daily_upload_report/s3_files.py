9132f2d96be13efa00a62a76316898293200e81b
david-fortini <fortini.david@gmail.com>
2023-06-13 15:22:26 +0200

    fixed timestmap bug

--- src/daily_upload_report/s3_files.py
+++ src/daily_upload_report/s3_files.py
@@ -17,10 +17,17 @@ import pandas as pd
 from collections import Counter
 bucket = 'docudeel-temp-storage'
 
+# item = {'Key': '13004SS_test_0_2023-06-13_13_07_27.csv', 'LastModified': datetime(2023, 6, 13, 13, 7, 29, tzinfo=timezone.utc), 'ETag': '"0226e9549ca78b631b19937b3f7ca4db"', 'Size': 243, 'StorageClass': 'STANDARD'}
+
+# n_days_last_modified = (datetime.now(timezone.utc) - item['LastModified'] ).days
+
+
+
 def is_recent_file(item: dict, n_days_ago: Optional[int] = None) -> bool:
     if n_days_ago is None:
         return True
-    n_days_last_modified = (item['LastModified'] - datetime.now(timezone.utc)).days
+    n_days_last_modified = (datetime.now(timezone.utc) - item['LastModified'] ).days
+    print(n_days_last_modified)
     is_recent = abs(n_days_last_modified) < n_days_ago
     return is_recent
 
@@ -47,7 +54,9 @@ def get_s3_files(bucket_name: str,
         if len(all_items) > 0:
             # apply transformations here!
             for item in all_items:
+                print(item)
                 is_recent = is_recent_file(item=item, n_days_ago=n_days_ago)
+                print(is_recent)
                 if is_recent:
                     all_file_names.append(item['Key'])
         if continuation_token is None:
@@ -73,4 +82,4 @@ def get_data(n_days_ago: int) -> pd.DataFrame:
 
 if __name__ == "__main__":
     data = get_data(1)
-    data = get_data(8)
+    # data = get_data(8)

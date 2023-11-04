04d813587297598526c822d7d14ce0de61d6f2e1
david-fortini <fortini.david@gmail.com>
2023-11-04 19:56:01 +0100

    okkk

--- src/config.py
+++ src/config.py
@@ -1,2 +1,2 @@
 DATA_FP = 'data/clean_relaties.csv' # used in prod
-SLACK_URL = "https://hooks.slack.com/services/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"
+SLACK_URL = "https://hooks.slack.com/services/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"

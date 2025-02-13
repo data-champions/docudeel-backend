68bee59fc5455f9a1a6bb7b18d1b10054327a427
david <fortini.david@gmail.com>
2025-02-13 16:24:04 +0100

    updated script

--- data/update_client_db.py
+++ data/update_client_db.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 
-excel_enrique_sent = "data/Kopie van clean_relaties(1) updated.xlsx"
+excel_enrique_sent = "data/Kopie van clean_relaties(1) updated.xlsx" # see whatsapp
 
 current_used_file = "data/clean_relaties.csv"
 
@@ -10,6 +10,7 @@ new = pd.read_excel(excel_enrique_sent)
 
 # new_output
 new.to_csv(current_used_file, index=False)
+new.to_excel('data/clean_relaties.xlsx', index=False)
 
 n_row = len(new)
 print(f'succesful. Total number contacts is: {n_row}')

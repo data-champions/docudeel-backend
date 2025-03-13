dd830c976ee07787d91ee20dd0d8bc67b2d34841
david <fortini.david@gmail.com>
2025-03-13 11:02:40 +0100

    updated docs

--- data/update_client_db.py
+++ data/update_client_db.py
@@ -2,13 +2,13 @@ import pandas as pd
 
 excel_enrique_sent = "data/Kopie van clean_relaties(1) updated.xlsx" # see whatsapp
 
-current_used_file = "data/clean_relaties.csv"
+current_used_file = "data/clean_relaties.csv" #used in prod
 
 df = pd.read_csv(current_used_file)
 
 new = pd.read_excel(excel_enrique_sent)
 
-# new_output
+# new prod output
 new.to_csv(current_used_file, index=False)
 new.to_excel('data/clean_relaties.xlsx', index=False)
 
